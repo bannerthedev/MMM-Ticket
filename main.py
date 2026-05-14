a7afaf8bca5c68a8c9c2f574aa93b5ad79929b7d
bannerthedev <leahsupan@hotmail.com>
2026-05-14 18:42:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def menu_embed():
         " before opening any ticket you need to know what you want. \n"
         "**Report A Player**\nReport a player for breaking the rules.\n"
         "**General Support**\nGet help with general questions or issues.\n"
-        "**Team's PFP's**/nsend a pfp of your team only PFP's
+        "**Team's PFP's**/nsend a pfp of your team only PFP's. /n"
     )
     return e
 
